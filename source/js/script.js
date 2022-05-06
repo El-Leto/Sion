14f42743c51164485530c56c1c893ec6531b2a0a
El-Leto <elleto.life@gmail.com>
2022-05-06 09:38:56 +0300

    fix

--- source/js/script.js
+++ source/js/script.js
@@ -20,7 +20,8 @@ const body = document.querySelector('body');
 let isMobile = {
 	Android: function() {return navigator.userAgent.match(/Android/i);},
 	BlackBerry: function() {return navigator.userAgent.match(/BlackBerry/i);},
-	iOS: function() {return navigator.userAgent.match(/iPhone|iPad|iPod/i);},
+	iOS: function() {return navigator.userAgent.match(/iPhone|iPad|iPod/i) ||
+  (NavigatorUAData.platform === 'MacIntel' && navigator.maxTouchPoints > 1);},
 	Opera: function() {return navigator.userAgent.match(/Opera Mini/i);},
 	Windows: function() {return navigator.userAgent.match(/IEMobile/i);},
 	any: function() {return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());}
