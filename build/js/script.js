0540dafc446823e7a37992b13c4883d7cc9ca48c
El-Leto <elleto.life@gmail.com>
2022-05-06 09:43:32 +0300

    fix

--- build/js/script.js
+++ build/js/script.js
@@ -20,11 +20,11 @@ const body = document.querySelector('body');
 let isMobile = {
 	Android: function() {return navigator.userAgent.match(/Android/i);},
 	BlackBerry: function() {return navigator.userAgent.match(/BlackBerry/i);},
-	iOS: function() {return navigator.userAgent.match(/iPhone|iPad|iPod/i) ||
-  (NavigatorUAData.platform === 'MacIntel' && navigator.maxTouchPoints > 1);},
+	iOS: function() {return navigator.userAgent.match(/iPhone|iPad|iPod/i);},
+  isIpad: function() {return NavigatorUAData.platform === 'MacIntel' && navigator.maxTouchPoints > 1;},
 	Opera: function() {return navigator.userAgent.match(/Opera Mini/i);},
 	Windows: function() {return navigator.userAgent.match(/IEMobile/i);},
-	any: function() {return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());}
+	any: function() {return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows() || isMobile.isIpad());}
 };
 
 isMobile.any() ? (body.classList.add('touch'), navDropdown.addEventListener('click', function() {
